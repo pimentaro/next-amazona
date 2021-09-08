ebeede542c2424f255db197be41dd9260b9476aa
pimentaro <gigadobrasil@gmail.com>
2021-09-08 09:07:10 -0400

    dark e light theme

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -47,7 +47,10 @@ export default function ProductScreen() {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1"> {product.name} </Typography>
+              <Typography variant="h1" component="h1">
+                {" "}
+                {product.name}{" "}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography> Category: {product.category} </Typography>
