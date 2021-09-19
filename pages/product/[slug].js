5638d662317e4c7b9b73e343bfaf96674d8d2981
pimentaro <gigadobrasil@gmail.com>
2021-09-19 18:35:09 -0400

    create Cart Screen

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -17,8 +17,10 @@ import db from "../../utils/db"
 import Product from "../../models/Product"
 import { Store } from "../../utils/Store"
 import axios from "axios"
+import { useRouter } from "next/router"
 
 export default function ProductScreen(props) {
+  const router = useRouter()
   const { dispatch } = useContext(Store)
   const { product } = props
   const classes = useStyles()
@@ -34,6 +36,7 @@ export default function ProductScreen(props) {
       return
     }
     dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } })
+    router.push("/cart")
   }
 
   return (
