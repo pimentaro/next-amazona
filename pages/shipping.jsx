a3915bec747237a75da477c201bf79ae360292fa
pimentaro <gigadobrasil@gmail.com>
2021-10-09 09:21:59 -0400

    get shipping Address

--- pages/shipping.jsx
+++ pages/shipping.jsx
@@ -1,14 +1,222 @@
-import React, { useContext } from "react"
-import { useRouter } from "next/router"
+import {
+  List,
+  Typography,
+  ListItem,
+  TextField,
+  Button,
+  Link,
+} from "@material-ui/core"
+import React, { useState, useContext, useEffect } from "react"
+import Layout from "../components/Layout"
+import useStyles from "../utils/styles"
+import NextLink from "next/link"
+
 import { Store } from "../utils/Store"
+import { useRouter } from "next/router"
+import Cookies from "js-cookie"
+import { Controller, useForm } from "react-hook-form"
+import CheckoutWizard from "../components/CheckoutWizard"
 
 export default function Shipping() {
-  const router = useRouter()
-  const { state, dispatch } = useContext(Store)
-  const { userInfo } = state
-  if (!userInfo) {
-    router.push("/login?redirect=/shipping")
-  }
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm()
 
-  return <div>Shipping</div>
+  const router = useRouter()
+  const { redirect } = router.query
+  const { state, dispatch } = useContext(Store)
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state
+  useEffect(() => {
+    if (!userInfo) {
+      router.push("/login?redirect=/shipping")
+    }
+    setValue("fullName", shippingAddress.fullName)
+    setValue("address", shippingAddress.address)
+    setValue("city", shippingAddress.city)
+    setValue("postalCode", shippingAddress.postalCode)
+    setValue("country", shippingAddress.country)
+  }, [])
+
+  const classes = useStyles()
+  const submitHandler = ({ fullName, address, city, postalCode, country }) => {
+    dispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: { fullName, address, city, postalCode, country },
+    })
+    Cookies.set(
+      "shippingAddress",
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    ),
+      router.push("/payment")
+  }
+  return (
+    <Layout title="Shipping">
+      <CheckoutWizard activeStep={1} />
+      <form className={classes.form} onSubmit={handleSubmit(submitHandler)}>
+        <Typography component="h1" variant="h1">
+          Shipping address
+        </Typography>
+        <List>
+          <ListItem>
+            <Controller
+              name="fullName"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="fullName"
+                  label="full Name"
+                  error={Boolean(errors.fullName)}
+                  helperText={
+                    errors.fullName
+                      ? errors.fullName.type === "minLength"
+                        ? "full Name length is more than 1"
+                        : "full Name is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="address"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="address"
+                  label="address"
+                  error={Boolean(errors.fullName)}
+                  helperText={
+                    errors.address
+                      ? errors.address.type === "minLength"
+                        ? "address length is more than 1"
+                        : "address is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="city"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="city"
+                  label="city"
+                  error={Boolean(errors.city)}
+                  helperText={
+                    errors.city
+                      ? errors.city.type === "minLength"
+                        ? "city length is more than 1"
+                        : "city is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="postalCode"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="postalCode"
+                  label="postal Code"
+                  error={Boolean(errors.postalCode)}
+                  helperText={
+                    errors.postalCode
+                      ? errors.postalCode.type === "minLength"
+                        ? "postal Code length is more than 1"
+                        : "postal Code is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+          <ListItem>
+            <Controller
+              name="country"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="country"
+                  label="country"
+                  error={Boolean(errors.country)}
+                  helperText={
+                    errors.country
+                      ? errors.country.type === "minLength"
+                        ? "country length is more than 1"
+                        : "country is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
+          </ListItem>
+
+          <ListItem>
+            <Button variant="contained" type="submit" fullWidth color="primary">
+              Continue
+            </Button>
+          </ListItem>
+        </List>
+      </form>
+    </Layout>
+  )
 }
--- utils/Store.js
+++ utils/Store.js
@@ -2,13 +2,19 @@ import { createContext, useReducer } from "react"
 import Cookies from "js-cookie"
 
 export const Store = createContext()
+console.log("oi")
+
 const initialState = {
   darkMode: Cookies.get("darkMode") === "ON" ? true : false,
   cart: {
     cartItems: Cookies.get("cartItems")
       ? JSON.parse(Cookies.get("cartItems"))
       : [],
+    shippingAddress: Cookies.get("shippingAddress")
+      ? JSON.parse(Cookies.get("shippingAddress"))
+      : {},
   },
+
   userInfo: Cookies.get("userInfo")
     ? JSON.parse(Cookies.get("userInfo"))
     : null,
@@ -40,6 +46,11 @@ function reducer(state, action) {
       Cookies.set("cartItems", JSON.stringify(cartItems))
       return { ...state, cart: { ...state.cart, cartItems } }
     }
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      }
     case "USER_LOGIN":
       return { ...state, userInfo: action.payload }
     case "USER_LOGOUT":
