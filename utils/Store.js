65be2c66c4cca7574ce3db170ff910e35fc5d42b
pimentaro <gigadobrasil@gmail.com>
2021-10-09 14:17:09 -0400

    create place order page

--- utils/Store.js
+++ utils/Store.js
@@ -51,6 +51,11 @@ function reducer(state, action) {
         ...state,
         cart: { ...state.cart, shippingAddress: action.payload },
       }
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
+      }
     case "USER_LOGIN":
       return { ...state, userInfo: action.payload }
     case "USER_LOGOUT":
